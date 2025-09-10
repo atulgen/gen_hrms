321d9a84332fbfad25fa20bdb95426c4567ae1ab
atulgen <atul.raj@gennextit.com>
2025-09-10 16:00:39 +0530

    Customization for Salary Slip

--- gen_hrms/hooks.py
+++ gen_hrms/hooks.py
@@ -8,6 +8,16 @@ app_license = "mit"
 # Apps
 # ------------------
 
+
+fixtures = [
+    "Custom Field",
+    "Server Script", 
+    "Client Script",
+    "Print Format",
+    "Property Setter"
+]
+
+
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
