23514b807ec567e8da61ae58df05a8a24e5fa0f9
atulgen <atul.raj@gennextit.com>
2025-09-12 12:01:03 +0530

    Intern Application and Offer

---
+++ gen_hrms/gen_hrms/doctype/intern_offer/intern_offer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, GennextIT and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Intern Offer", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ gen_hrms/gen_hrms/doctype/intern_offer/test_intern_offer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, GennextIT and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestInternOffer(FrappeTestCase):
+	pass
